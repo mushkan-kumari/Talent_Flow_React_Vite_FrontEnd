002530b9a0c8f2a47f97bffdfc081d813c20ea0c
mushkan-kumari <mushkank1307@gmail.com>
2025-10-08 23:53:20 +0530

    Final Edit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/Talent_Flow_React_Application",
+  base: process.env.VITE_BASE_PATH || "/Talent_Flow_React_Vite_FrontEnd",
 })
